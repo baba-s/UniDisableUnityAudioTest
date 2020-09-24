6a00f7b60e7667ecebfb352661470ba730e13760
baba_s <shotababa1206@gmail.com>
2020-09-24 19:59:03 +0900

    first commit

---
+++ Editor/DisableUnityAudioTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using UnityEditor;
+
+namespace Kogane.Internal
+{
+	internal sealed class DisableUnityAudioTest
+	{
+		[Category( nameof( Kogane ) )]
+		[Test]
+		public void Disable_Unity_Audio_が有効になっているか()
+		{
+			var path             = "ProjectSettings/AudioManager.asset";
+			var manager          = AssetDatabase.LoadAllAssetsAtPath( path ).FirstOrDefault();
+			var serializedObject = new SerializedObject( manager );
+			var property         = serializedObject.FindProperty( "m_DisableAudio" );
+
+			Assert.IsTrue( property.boolValue );
+		}
+	}
+}
